aef387d37957cd96858c517adab71218940f22a7
GitBolt <stingbolt1@gmail.com>
2021-09-06 11:38:47 +0530

    📄 Added index page

--- python/server/main.py
+++ python/server/main.py
@@ -1,7 +1,9 @@
 import os
+from typing import Union
 from nacl.signing import VerifyKey
 from fastapi import FastAPI, Request, HTTPException
 from nacl.exceptions import BadSignatureError
+from starlette.responses import JSONResponse
 
 # Your public key can be found on your application in the Developer Portal
 PUBLIC_KEY = os.environ["PUBLIC_KEY"]
@@ -22,15 +24,23 @@ def verify_key(signature: str, timestamp: str, body: str) -> bool:
         return False
 
 
-@app.post("/")
-async def index(request: Request) -> str:
+@app.get("/")
+async def index() -> JSONResponse:
+    return JSONResponse(
+        status_code=200, 
+        content={"Message": "Welcome to interaction webhook server"}
+        )
+        
+        
+@app.post("/interactions")
+async def index(request: Request) -> Union[JSONResponse, HTTPException]:
     
     signature = request.headers["X-Signature-Ed25519"]
     timestamp = request.headers["X-Signature-Timestamp"]
     body = request.body.decode("utf-8")
 
     if verify_key(signature, timestamp, body):
-        return request.json()
+        return JSONResponse(status_code=200, content=request.json())
     else:
         return HTTPException(
             status_code=401, detail={"error": "Incorrect request"}
